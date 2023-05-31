4c522e6d8b1467ff17bc53e9e7e444cad7b536f0
matela <jmatelaneto@gmail.com>
2023-05-31 14:27:17 -0300

    creating the api call function

--- inmetrics-challenge/Service/UserService.swift
+++ inmetrics-challenge/Service/UserService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class UserService {
+    let baseURL: String = "https://api.github.com/users"
+    
+    //MARK: TODO - 1. Verificar e criar paginacao para listagem de usuarios
+    
+    func fetchUsers(completion: @escaping ([User]) -> ()) {
+        let url = URL(string: self.baseURL)!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            print("RESPONSE: \(String(describing: response))")
+            print("ERROR: \(String(describing: error?.localizedDescription))")
+            
+            if let data = data {
+                let users = try? JSONDecoder().decode([User].self, from: data)
+                completion(users!)
+            }
+        }
+        task.resume()
+    }
+}
