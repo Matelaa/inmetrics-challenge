c9a2a00503940c77c248c20fa4d13cdd8aa80426
matela <jmatelaneto@gmail.com>
2023-05-31 14:27:50 -0300

    creating the logic to call the api and get users list

--- inmetrics-challenge/ViewModel/UserViewModel.swift
+++ inmetrics-challenge/ViewModel/UserViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol UserViewModelDelegate {
+    func getUsersList()
+}
+
+class UserViewModel {
+    
+    var delegate: UserViewModelDelegate!
+    let service = UserService()
+    var users = [User]()
+    
+    func getUsers() {
+        self.service.fetchUsers { users in
+            self.users.append(users.first!)
+            self.delegate.getUsersList()
+        }
+    }
+}
