bbe6874d501583c412b4774f7d8dc25f3bc81208
matela <jmatelaneto@gmail.com>
2023-06-01 14:44:11 -0300

    creating a loading screen when call api, and their logic to show and remove

--- inmetrics-challenge/Controllers/UserListViewController.swift
+++ inmetrics-challenge/Controllers/UserListViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class UserListViewController: UIViewController {
-
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.center = self.view.center
+        activityIndicator.color = .gray
+        activityIndicator.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
+        activityIndicator.startAnimating()
+        
+        return activityIndicator
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -20,10 +31,22 @@ class UserListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .white
         
         self.viewModel.delegate = self
+        
+        self.setupUI()
+        
         self.viewModel.getUsers()
+    }
+    
+    private func setupLoadingScreen() {
+        self.view.addSubview(self.activityIndicator)
+        self.setupActivityIndicatorConstraints()
+    }
+    
+    private func removeLoadingScreen() {
+        self.activityIndicator.removeFromSuperview()
     }
     
     private func setupUI() {
@@ -41,13 +64,28 @@ class UserListViewController: UIViewController {
         self.tableView.separatorStyle = .none
     }
     
+    private func reloadTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
     private func setupConstraints() {
         self.setupTableViewConstraints()
     }
     
+    private func setupActivityIndicatorConstraints() {
+        NSLayoutConstraint.activate([
+            self.activityIndicator.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.activityIndicator.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.activityIndicator.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.activityIndicator.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+        ])
+    }
+    
     private func setupTableViewConstraints() {
         NSLayoutConstraint.activate([
-            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             self.tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
             self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
@@ -75,22 +113,25 @@ extension UserListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("click on cell: \(indexPath.item)")
+        print(self.viewModel.users[indexPath.item].login)
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let lastRowIndex = tableView.numberOfRows(inSection: 0) - 1
         if indexPath.row == lastRowIndex {
-            print("tableView final row")
+            self.viewModel.getUsers()
         }
     }
 }
 
 extension UserListViewController: UserViewModelDelegate {
-    func getUsersList() {
-        DispatchQueue.main.async {
-            self.setupUI()
-            self.tableView.reloadData()
+    
+    func onIsLoadChange() {
+        if self.viewModel.isLoading {
+            self.setupLoadingScreen()
+        } else {
+            self.removeLoadingScreen()
+            self.reloadTableView()
         }
     }
 }
